c5ec03cb516bb8ad2f8ba410120d5d0a555134ed
Raquel Alvarez <alu0100700435@ull.edu.es>
2014-09-26 09:30:38 +0100

    recuperando antigua version

--- twitter.rb
+++ twitter.rb
@@ -1,43 +1,40 @@
-# -*- coding: utf-8 -*-
 require 'twitter'
 require 'sinatra'
-
 require './configure'
-
-get '/' do  
-  @fc = 0 # numero de amigos
-  @name = ''  #nombre de usuario twitter
-  @usuarios = Hash.new	#almacenara los amigos y el numero de seguidores
-  @users = []
-  erb :twitter
+get '/' do
+    @fc = 0 # numero de amigos
+    @name = '' #nombre de usuario twitter
+    @usuarios = Hash.new	#almacenara los amigos y el numero de seguidores
+    @users = [] #contendra los amigos ordenados
+    erb :twitter
 end
-
-post '/' do  
-  @fc = 0   
-  @usuarios = Hash.new
-  @users = []
-  @name = params[:firstname] || ''  #recoge del parametro firstname el nombre de usuario
-  client = my_twitter_client()  #establece conexion twitter
-  
-  if client.user? @name #si el usuario introducido es de twitter 
-    @fc = client.user(@name).friends_count #recoge el numero de amigos del usuario
-        
-    if (@fc <=10) #si tiene menos de 10 amigos
-        amigos = client.friend_ids(@name).attrs[:ids].take(@fc) #coge tantos amigos como tiene el usuario segun su atributo id
-        @fc.times do |i|            
-            amigos.map{@usuarios[client.user(amigos[i]).name] = client.user(amigos[i]).followers_count.to_i} 
-        end 
+post '/' do
+    @fc = 0
+    @usuarios = Hash.new #almacenara los amigos y el numero de seguidores
+    @name = params[:firstname] || '' #recoge del parametro firstname el nombre de usuario
+    @users = []  #contendra los amigos ordenados 
+    client = my_twitter_client() #establece conexion twitter
+    
+    if client.user? @name #si el usuario introducido es de twitter
+        a_user = client.user(@name)
+        @fc = a_user.friends_count #recoge el numero de amigos del usuario
+         
+        if (@fc <= 10) #si tiene menos de diez amigos
+            amigos = client.friend_ids(@name).attrs[:ids].take(@fc) #coge fc amigos del usuario segun su atributo id
+            @fc.times do |i|
+                user_n = client.user(amigos[i])
+                @usuarios[user_n.screen_name.to_s] = user_n.followers_count.to_i
+            end
+        end
+        if (@fc > 10) #si tiene mas de diez amigos
+            amigos = client.friend_ids(@name).attrs[:ids].take(10) #coge 10 amigos del usuario segun su atributo id
+            10.times do |i|
+                user_n = client.user(amigos[i])
+                @usuarios[user_n.screen_name.to_s] = user_n.followers_count.to_i
+            end
+        end
+        @users = @usuarios.sort_by {|k,v| -v} #ordena los amigos de mayor a menor segun sus seguidores
     end
-    
-    if (@fc > 10) #si tiene mas de 10 amigos  
-        amigos = client.friend_ids(@name).attrs[:ids].take(10) #coge 20 amigos  del usuario segun su atributo id
-        10.times do |i|            
-            amigos.map{@usuarios[client.user(amigos[i]).name] = client.user(amigos[i]).followers_count.to_i} 
-        end         
-    end 
-    
-    @users = @usuarios.sort_by {|k,v| -v} #ordena los amigos de mayor a menor segun sus seguidores              
-  end
-  erb :twitter
+    erb :twitter
 end
  
